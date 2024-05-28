29e1494e1634f89cf48c5c8b32d5ff4e8ca82ac4
Alimul Hasan Jami <alimulhasan000@gmail.com>
2024-05-28 11:22:08 -0400

    2nd commit - model with layers added, test.yaml file is added in workflows folder

--- layered_model.py
+++ layered_model.py
@@ -8,7 +8,13 @@ def define_dense_model_single_layer(input_length, activation_f='sigmoid', output
     input_length: the number of inputs
     activation_f: the activation function
     output_length: the number of outputs (number of neurons)"""
-    # model = None
+
+    model = keras.Sequential()  # create a sequential model.
+    model.add(layers.Input(shape=(input_length,)))  # add an input layer.
+    model.add(layers.Dense(output_length, activation=activation_f))     # add a dense layer(most common & frequently used).
+    # Dense layer is a regular densely-connected NN layer. https://keras.io/api/layers/core_layers/dense/
+
+    return model
 
 def define_dense_model_with_hidden_layer(input_length, 
                                          activation_func_array=['relu','sigmoid'],
@@ -20,20 +26,28 @@ def define_dense_model_with_hidden_layer(input_length,
     hidden_layer_size: the number of neurons in the hidden layer
     output_length: the number of outputs (number of neurons in the output layer)"""
 
-    model = None
+    model = keras.Sequential()  # create a sequential model - a linear stack of layers. https://keras.io/guides/sequential_model/
+    model.add(layers.Input(shape=(input_length,)))  # add an input layer.
+    model.add(layers.Dense(hidden_layer_size, activation=activation_func_array[0]))  # add a hidden layer.
+    model.add(layers.Dense(output_length, activation=activation_func_array[1]))  # add an output layer.
+
     return model
 
 
 def get_mnist_data():
     """Get the MNIST data."""
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    x_train = x_train.reshape(60000, 784).astype('float32') / 255
-    x_test = x_test.reshape(10000, 784).astype('float32') / 255 
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()    # load the mnist data.
+    x_train = x_train.reshape(60000, 784).astype('float32') / 255   # reshape the train data.
+    x_test = x_test.reshape(10000, 784).astype('float32') / 255     # reshape the test data.
     return (x_train, y_train), (x_test, y_test)
 
 def binarize_labels(labels, target_digit=2):
     """Binarize the labels."""
-    labels = 1*(labels==target_digit)
+    labels = 1*(labels==target_digit)   # binarize the labels by checking if the label is equal to the target_digit.
+    # How this Binarizatio works:
+    # if the label is equal to the target_digit, it will return 1, else it will return 0.
+    # e.g: if target_digit 2, then labels will be binarized as follows: 2 -> 1, 3 -> 0, 4 -> 0, 5 -> 0,...
+
     return labels
 
 def fit_mnist_model_single_digit(x_train, y_train, target_digit, model, epochs=10, batch_size=128):
@@ -43,13 +57,17 @@ def fit_mnist_model_single_digit(x_train, y_train, target_digit, model, epochs=1
 
     then fit the model on the training data. (pass the epochs and batch_size params)
     """
-    y_train = binarize_labels(y_train, target_digit)
+    y_train = binarize_labels(y_train, target_digit)    # binarize the labels for the target_digit.
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])  # compile the model.
+    model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size)   # fit the model on the training data.
+
     return model
 
 def evaluate_mnist_model_single_digit(x_test, y_test, target_digit, model):
     """Evaluate the model on the test data.
     Hint: use model.evaluate() to evaluate the model on the test data.
     """
-    y_test = binarize_labels(y_test, target_digit)
-    loss, accuracy = None, None  ## change this.
+    y_test = binarize_labels(y_test, target_digit)  # binarize the labels for the target_digit.
+    loss, accuracy = model.evaluate(x_test, y_test) # evaluate the model on the test data.
+
     return loss, accuracy
--- layered_model_test.py
+++ layered_model_test.py
@@ -8,25 +8,26 @@ from layered_model import fit_mnist_model_single_digit, evaluate_mnist_model_sin
 from layered_model import binarize_labels, get_mnist_data
 
 def test_define_dense_model_single_layer():
-    model = define_dense_model_single_layer(43, activation_f='sigmoid', output_length=1)
-    assert len(model.layers) == 1, " model should have 1 layer"
-    assert model.layers[0].input_shape == (None, 43), " input_shape is not correct"
-    assert model.layers[0].output_shape == (None, 1), " output_shape is not correct"
+    model = define_dense_model_single_layer(43, activation_f='sigmoid', output_length=1)    # create a model with 43 inputs, sigmoid activation function, and 1 output.
+    assert len(model.layers) == 1, " model should have 1 layer"     # check if the model has 1 layer.
+    assert model.layers[0].input_shape == (None, 43), " input_shape is not correct"     # check if the input_shape is correct.
+    assert model.layers[0].output_shape == (None, 1), " output_shape is not correct"    # check if the output_shape is correct.
 
 
 def test_define_dense_model_with_hidden_layer():
+    # create a model with 43 inputs, activation function: relu - hidden layer, sigmoid - output layer, 11 neurons in the hidden layer, and 1 output.
     model = define_dense_model_with_hidden_layer(43, activation_func_array=['relu','sigmoid'], hidden_layer_size=11, output_length=1)
-    assert len(model.layers) == 2, " model should have 2 layers"
+    assert len(model.layers) == 2, " model should have 2 layers"    # check if the model has 2 layers.
     assert model.layers[0].input_shape == (None, 43), " input_shape is not correct"
     assert model.layers[0].output_shape == (None, 11), " output_shape is not correct"
     assert model.layers[1].output_shape == (None, 1), " output_shape is not correct"
 
 
 def test_fit_and_predict_mnist_single_digit_one_neuron():
-    model = define_dense_model_single_layer(28*28, activation_f='sigmoid', output_length=1)
-    (x_train, y_train), (x_test, y_test) = get_mnist_data()
-    model = fit_mnist_model_single_digit(x_train, y_train, 2, model, epochs=5, batch_size=128)
-    loss, accuracy = evaluate_mnist_model_single_digit(x_test, y_test, 2, model)
+    model = define_dense_model_single_layer(28*28, activation_f='sigmoid', output_length=1)     # create a model with 28*28 inputs, sigmoid activation function, and 1 output.
+    (x_train, y_train), (x_test, y_test) = get_mnist_data() # get the mnist data.
+    model = fit_mnist_model_single_digit(x_train, y_train, 2, model, epochs=5, batch_size=128)  # fit the model to the data.
+    loss, accuracy = evaluate_mnist_model_single_digit(x_test, y_test, 2, model)    # evaluate the model on the test data.
     assert accuracy > 0.9, " accuracy should be greater than 0.9"
-    loss, accuracy = evaluate_mnist_model_single_digit(x_test, y_test, 3, model)
-    assert accuracy < 0.9, " accuracy should be smaller than 0.9"
+    loss, accuracy = evaluate_mnist_model_single_digit(x_test, y_test, 3, model)    # evaluate the model on the test data for a different digit.
+    assert accuracy < 0.9, " accuracy should be smaller than 0.9"   # check if the accuracy is less than 0.9.
